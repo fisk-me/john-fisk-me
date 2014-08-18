facf75b90f188e4b85032b8566d8ffd8b5639c7d
johnpfisk <johnpfisk@gmail.com>
2014-08-18 07:24:22 +0000

    seo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import webapp2
+import urllib
 from google.appengine.ext.webapp import template
+from google.appengine.api import urlfetch
 
 class RobotsPage(webapp2.RequestHandler):
     def get(self):
@@ -12,8 +14,12 @@ class IndexPage(webapp2.RequestHandler):
         fragment = self.request.get('_escaped_fragment_')
 
         if fragment:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write( fragment )
+            result = urlfetch.fetch("https://spreadsheets.google.com/feeds/list/1-duH2HS3Y_mjbXRib3mxzjwaxfPmZ7QQvVcqwH_jeZQ/1/public/full?sq=reference="+ urllib.quote( fragment.split('-')[0] ) +"&alt=atom")
+            if result.status_code == 200:
+                self.response.headers['Content-Type'] = 'application/atom+xml'
+                self.response.out.write(result)
+            else:
+                self.response.status = 404
         else:
             self.response.headers['Content-Type'] = 'text/html'
             self.response.out.write(open('index.html','r').read())
@@ -22,3 +28,5 @@ app = webapp2.WSGIApplication([
     ('/robots.txt', RobotsPage),
     (r'/.*', IndexPage)
 ], debug=True)
+
+
