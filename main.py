02d5e78a64955db21470c1dd04657ccf7eb614df
johnpfisk <johnpfisk@gmail.com>
2014-08-18 12:39:59 +0000

    seo

--- main.py
+++ main.py
@@ -8,30 +8,33 @@ from google.appengine.api import urlfetch
 
 class RobotsPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        result = urllib2.urlopen('https://spreadsheets.google.com/feeds/cells/1-duH2HS3Y_mjbXRib3mxzjwaxfPmZ7QQvVcqwH_jeZQ/1/public/basic?prettyprint=true&min-col=1&max-col=2&alt=json')
+        self.response.headers["Content-Type"] = "text/plain"
+        self.response.out.write( "User-Agent: *\n" )
+        self.response.out.write( "Disallow:\n" )
+
+        result = urllib2.urlopen("https://spreadsheets.google.com/feeds/cells/1-duH2HS3Y_mjbXRib3mxzjwaxfPmZ7QQvVcqwH_jeZQ/1/public/basic?prettyprint=true&min-col=1&max-col=2&alt=json")
         html = result.read()
         html = json.loads(html)
 
-        for entry in html['feed']['entry']:
-            self.response.out.write( entry['content']['$t'].encode('utf-8').strip() )
+        for entry in range(0,len(html["feed"]["entry"])):
+            self.response.out.write( "Allow: " + html["feed"]["entry"][entry*2]["content"]["$t"].encode("utf-8").strip() + "-" + html["feed"]["entry"][entry*2+1]["content"]["$t"].encode("utf-8").strip())
 
 class IndexPage(webapp2.RequestHandler):
     def get(self):
-        fragment = self.request.get('_escaped_fragment_')
+        fragment = self.request.get("_escaped_fragment_")
 
         if fragment:
-            result = urlfetch.fetch("https://spreadsheets.google.com/feeds/list/1-duH2HS3Y_mjbXRib3mxzjwaxfPmZ7QQvVcqwH_jeZQ/1/public/full?sq=uniqueid="+ urllib.quote( fragment.split('-')[0] ) +"&alt=atom")
+            result = urlfetch.fetch("https://spreadsheets.google.com/feeds/list/1-duH2HS3Y_mjbXRib3mxzjwaxfPmZ7QQvVcqwH_jeZQ/1/public/full?sq=uniqueid="+ urllib.quote( fragment.split("-")[0] ) +"&alt=atom")
             if result.status_code == 200:
-                self.response.headers['Content-Type'] = 'application/atom+xml'
+                self.response.headers["Content-Type"] = "application/atom+xml"
                 self.response.out.write(result.content)
             else:
                 self.response.status = 404
         else:
-            self.response.headers['Content-Type'] = 'text/html'
-            self.response.out.write(open('index.html','r').read())
+            self.response.headers["Content-Type"] = "text/html"
+            self.response.out.write(open("index.html","r").read())
 
 app = webapp2.WSGIApplication([
-    ('/robots.txt', RobotsPage),
-    (r'/.*', IndexPage)
+    ("/robots.txt", RobotsPage),
+    (r"/.*", IndexPage)
 ], debug=True)
