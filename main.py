390c5d350401e3edf36b36fa558259c0da2d54d4
johnpfisk <johnpfisk@gmail.com>
2014-08-19 00:25:56 +0000

    seo

--- main.py
+++ main.py
@@ -4,26 +4,25 @@ import urllib
 import urllib2
 import json
 from google.appengine.ext.webapp import template
-from google.appengine.api import urlfetch
 
 class SitemapPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers["Content-Type"] = "text/plain"
         result = urllib2.urlopen("https://spreadsheets.google.com/feeds/cells/1-duH2HS3Y_mjbXRib3mxzjwaxfPmZ7QQvVcqwH_jeZQ/1/public/basic?prettyprint=true&min-col=1&max-col=2&alt=json")
-        html = result.read()
-        html = json.loads(html)
-        for entry in range(1,len(html["feed"]["entry"])/2):
-            self.response.out.write( "http://john.fisk.me/#!" + html["feed"]["entry"][entry*2]["content"]["$t"].encode("utf-8").strip() + "-" + urllib.quote(html["feed"]["entry"][entry*2+1]["content"]["$t"].encode("utf-8").strip()) +"\n" )
+        data = json.loads(result.read())
+        for entry in range(1,len(data["feed"]["entry"])/2):
+            self.response.out.write( "http://john.fisk.me/#!" + data["feed"]["entry"][entry*2]["content"]["$t"].encode("utf-8").strip() + "-" + urllib.quote(data["feed"]["entry"][entry*2+1]["content"]["$t"].encode("utf-8").strip()) +"\n" )
 
 class IndexPage(webapp2.RequestHandler):
     def get(self):
-        fragment = self.request.get("_escaped_fragment_")
+        fragment = self.request.get("_escaped_fragment_") + "-"
 
         if fragment:
-            result = urllib2.urlopen("https://spreadsheets.google.com/feeds/list/1-duH2HS3Y_mjbXRib3mxzjwaxfPmZ7QQvVcqwH_jeZQ/1/public/full?sq=uniqueid="+ urllib.quote( fragment.split("-")[0] ) +"&alt=atom")
+            result = urllib2.urlopen("https://spreadsheets.google.com/feeds/list/1-duH2HS3Y_mjbXRib3mxzjwaxfPmZ7QQvVcqwH_jeZQ/1/public/full?sq=uniqueid="+ urllib.quote( fragment.split("-")[0] ) +"&prettyprint=true&alt=json")
             if result.getcode() == 200:
-                self.response.headers["Content-Type"] = "application/atom+xml"
-                self.response.out.write(result.read())
+                data = json.loads(result.read())
+                self.response.headers["Content-Type"] = 01"application/atom+xml"
+                self.response.out.write(template.render("entry.tmpl", data))
             else:
                 self.response.status = 404
         else:
